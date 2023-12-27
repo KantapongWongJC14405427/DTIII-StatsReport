1af9ca7a832a81686c6a6c7a652bb2e131af247d
KantapongWongJC14405427 <kantapong.wongsanguan@my.jcu.edu.au>
2023-12-27 09:20:12 +0800

    Commit

--- db.sql
+++ db.sql
@@ -1,24 +1,24 @@
--- CREATE TABLE `users` (
---   `id` integer PRIMARY KEY,
---   `username` varchar(255),
---   `role` varchar(255)
--- );
+CREATE TABLE `users` (
+  `id` integer PRIMARY KEY,
+  `username` varchar(255),
+  `role` varchar(255)
+);
 
--- CREATE TABLE `users_stats` (
---   `id` integer PRIMARY KEY,
---   `title` varchar(255),
---   `user_id` integer,
---   `status` varchar(255),
---   `user_questions_done` integer
--- );
+CREATE TABLE `users_stats` (
+  `id` integer PRIMARY KEY,
+  `title` varchar(255),
+  `user_id` integer,
+  `status` varchar(255),
+  `user_questions_done` integer
+);
 
--- CREATE TABLE `users_survey` (
---   `id` integer PRIMARY KEY,
---   `user_id` integer,
---   `title` varchar(255),
---   `max_question` integer
--- );
+CREATE TABLE `users_survey` (
+  `id` integer PRIMARY KEY,
+  `user_id` integer,
+  `title` varchar(255),
+  `max_question` integer
+);
 
--- ALTER TABLE `users_stats` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `users_stats` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
--- ALTER TABLE `users_survey` ADD FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+ALTER TABLE `users_survey` ADD FOREIGN KEY (`id`) REFERENCES `users` (`id`);
